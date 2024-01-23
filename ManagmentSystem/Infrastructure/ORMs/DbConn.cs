131a3297a1d6daf72f61dadc4bd0f689a7a6242a
ahmedsalehalzabr <ahmedalzabr1@gmail.com>
2024-01-23 18:16:22 +0500

    Add project files.

---
+++ ManagmentSystem/Infrastructure/ORMs/DbConn.cs
@@ -0,0 +1,23 @@
+﻿namespace Infrastructure.ORM;
+
+public enum ConnectionType
+{
+    SqlServer,
+    PostgreSQL,
+}
+
+public static class DBConn
+{
+    public static readonly ConnectionType ConnectionType = ConnectionType.SqlServer;
+
+    public static string ConnectionString => ConnectionType switch
+    {
+        ConnectionType.SqlServer => "Server=DESKTOP-CKRA04M\\SQLEXPRESS; Database=ManagmentSystem; " +
+            "Trusted_Connection=True; MultipleActiveResultSets=true; User Id=sa; Password=771143849; TrustServerCertificate=True;",
+
+        ConnectionType.PostgreSQL => "Server=localhost; Port=5432; Database=ManagmentSystem; " +
+            "User Id=postgres; Password=postgres;",
+
+        _ => "",
+    };
+}
